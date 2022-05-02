c3cb2972e030d566fa14a036b2db5828d630ed61
lck0107 <dlckdrl10720@gmail.com>
2022-05-02 23:15:46 +0900

    ~sixthweek

--- python+/crawlerCH2.py
+++ python+/crawlerCH2.py
@@ -1,19 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 url = "https://www.daum.net/"
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-print(response.text)
+# print(response.text)
 
-*--
-file = open("daum.html","w")
-file.write(response.text)
-find.close()
 
-print(soup.title)
-print(soup.title.string)
-print(soup.span)
-print(soup.findall('span'))
---*
+#file = open("daum.html","w") file.write(response.text)
+#find.close()
+
+#print(soup.title)
+#print(soup.title.string)
+#print(soup.span)
+#print(soup.findall('span'))
+
+rank = 1
+
+results = soup.findall('a','link_favorsch')
+
+search_rank_file = open("rankresult.txt","a")
+
+print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
+
+for result in results:
+    search_rank_file.write(str(rank)+"위:"+result.get_text()+"\n")
+    rank += 1
